c20cca8f87024f1fa60879940e7a51902cb102b5
Tsuki-gakirei <851456759@qq.com>
2021-05-24 13:57:07 +0800

    complete proj1b v1

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,8 +10,13 @@ public class TestOffByOne {
     // Your tests go here.
     @Test
     public void testEqualChars() {
-        assertFalse(offByOne.equalChars('a','a'));
-        assertFalse(offByOne.equalChars('a','z'));
-        assertTrue(offByOne.equalChars('&','%'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('A', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
     }
 }
